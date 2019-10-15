29d12a37b5220ca5702108c723a32ebb2cf49939
Aman Jain <amanjain.3331@gmail.com>
2019-10-15 12:00:42 +0530

    firsty commit

---
+++ precache-manifest.b9ce56d5a6497ca09abcb351b7f5eac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "a7cf5ce38dfdcc7c8650",
+    "url": "/static/js/main.a7cf5ce3.chunk.js"
+  },
+  {
+    "revision": "480a53c8bebb7cbb729b",
+    "url": "/static/js/2.480a53c8.chunk.js"
+  },
+  {
+    "revision": "a7cf5ce38dfdcc7c8650",
+    "url": "/static/css/main.8a8636cc.chunk.css"
+  },
+  {
+    "revision": "480a53c8bebb7cbb729b",
+    "url": "/static/css/2.64d4f9aa.chunk.css"
+  },
+  {
+    "revision": "5ec9396cd02c4e7e611f0b5aa448fa25",
+    "url": "/index.html"
+  }
+];
